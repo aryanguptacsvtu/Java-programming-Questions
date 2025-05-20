67b438d149653fc79968c5688abe314fc6bf57ec
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-20 17:06:42 +0530

    Java Stdin and Stdout I

---
+++ HackerRank QUESTIONS/EASY/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Java_Stdin_and_Stdout_I {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
+
+/* " Java Stdin and Stdout I":-
+
+"Task":-
+In this challenge, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line.
+
+"Input Format":-
+There are  lines of input, and each line contains a single integer.
+
+"Sample Input":-
+42
+100
+125
+
+"Sample Output":-
+42
+100
+125
+ */
